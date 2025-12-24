def66f427ccfed13e305f65d19c174d01d8e730e
abdullah cihan <abdullahcihan21@gmail.com>
2025-12-24 23:11:48 +0300

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // === API URL'İNİZİ BURAYA YAPIŞTIRIN ===
-    const API_URL = "https://script.google.com/macros/s/AKfycbyfxBUq0d-sj315o5a_tgS76h0hDMvJKwFhrGzdnGJXKHDKp9oabootgeyCn9QQJ_2fdw/exec";
+    const API_URL = "https://script.google.com/macros/s/AKfycbywMO77KzYzCJWFhlyru8_YVaE8QBmOj-7n5FK0m1lL6BCSt4oEBcxOuxedJeNSbUBg7w/exec";
 
     const toolsContainer = document.getElementById('tools-list-container');
     if (!toolsContainer) return;
@@ -29,3 +29,4 @@ document.addEventListener('DOMContentLoaded', () => {
             toolsContainer.innerHTML = '<div class="tool-empty">Yüklenemedi.</div>';
         });
 });
+
