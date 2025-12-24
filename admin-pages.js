2bea1dcb2e24a21d27ccf45f7026edad0261db67
abdullah cihan <abdullahcihan21@gmail.com>
2025-12-24 23:08:59 +0300

    Update admin-pages.js

--- admin-pages.js
+++ admin-pages.js
@@ -5,7 +5,7 @@
   // AYARLAR
   // ==========================================
   // GÜNCEL API LİNKİ:
-  const API_URL = "https://script.google.com/macros/s/AKfycbyu8zJD218qrZoIGX3BmjGml4KhBultnL1oz1X7lgyrxP9vUC6LfvQ8K-IjZvB6XBu89g/exec";
+  const API_URL = "https://script.google.com/macros/s/AKfycbywMO77KzYzCJWFhlyru8_YVaE8QBmOj-7n5FK0m1lL6BCSt4oEBcxOuxedJeNSbUBg7w/exec";
 
 
   // ----------------------------
