b2f4f12b33e0fd14e5884c79c208c6074f71e4cf
abdullah cihan <abdullahcihan21@gmail.com>
2025-12-25 23:49:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -104,5 +104,69 @@ document.addEventListener('DOMContentLoaded', async () => {
           progressBar.style.width = scrolled + "%";
       }
   });
+  /* =========================================
+   DAKTİLO EFEKTİ (TYPEWRITER)
+   ========================================= */
+
+const TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+// Yazma Döngüsü (Tick)
+TxtType.prototype.tick = function() {
+    // Şu anki kelime dizisini al
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    // Siliniyor mu yazılıyor mu?
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // HTML'e yaz
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    // Hız ayarları (Delta)
+    var that = this;
+    var delta = 200 - Math.random() * 100; // Rastgele yazma hızı
+
+    if (this.isDeleting) { delta /= 2; } // Silerken daha hızlı olsun
+
+    // Kelime tamamlandıysa
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period; // Bekleme süresi (data-period)
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500; // Yeni kelimeye geçmeden önce kısa bekleme
+    }
+
+    // Döngüyü tekrar çağır
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+// Sayfa Yüklendiğinde Başlat
+window.addEventListener('load', function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+});
   // ------------------------------------------------
 });
+
